a60f64bce3032d4b56728d27cb2a925233c174a4
Goksel Baltaci <gokselbaltaci01@gmail.com>
2022-04-26 19:20:06 +0300

    Searchbox style tamamlandı

---
+++ src/components/shared_component/SearchBox/SearchBox.style.js
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+export default StyleSheet.create({
+  container: {
+    marginVertical: 5,
+  },
+  input: {
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 10,
+    fontSize: 15,
+    padding: 5,
+  },
+});
