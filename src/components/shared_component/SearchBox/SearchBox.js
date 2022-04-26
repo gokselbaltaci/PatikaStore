e0cfd3fe27e452141d113340c09f987d2b5cfe57
Goksel Baltaci <gokselbaltaci01@gmail.com>
2022-04-26 19:19:58 +0300

    SearchBox tamamlandı

---
+++ src/components/shared_component/SearchBox/SearchBox.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {TextInput, View} from 'react-native';
+import styles from './SearchBox.style';
+export default function SearchBox() {
+  return (
+    <View style={styles.container}>
+      <TextInput style={styles.input} placeholder="Search..." />
+    </View>
+  );
+}
