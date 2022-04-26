6449c2dd3307b8a4e19c2e50759632b3633110f5
Goksel Baltaci <gokselbaltaci01@gmail.com>
2022-04-26 18:37:55 +0300

    Header style yapıldı

---
+++ src/components/shared_component/Header/Header.style.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    marginVertical: 5,
+  },
+  title: {
+    color: '#610094',
+    fontSize: 28,
+    fontWeight: 'bold',
+  },
+});
