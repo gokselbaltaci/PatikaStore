492b9239d7d7b36738cccf765608e5730aa0916a
Goksel Baltaci <gokselbaltaci01@gmail.com>
2022-04-26 18:42:08 +0300

    Header düzeltildi

---
+++ src/components/shared_component/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import styles from './Header.style';
+
+export default function Header() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>PatikaStore</Text>
+    </View>
+  );
+}
